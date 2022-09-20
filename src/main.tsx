1ad242486e2d003392607f3a06c7b182483f4772
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-20 01:54:26 -0300

    fix: add chakra ui and radio stream providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { RadioStreamProvider } from './providers/streamAudio'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+    <RadioStreamProvider>
+      <App />
+    </RadioStreamProvider>
+  </ChakraProvider>
 )
