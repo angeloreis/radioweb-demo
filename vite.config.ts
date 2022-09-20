7ffb6a12c3a6b9fdbf35204d1b82c24fb1da80a2
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-09-20 01:53:10 -0300

    fix: add some deps and config of vitejs

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    hmr: {
+        host: 'localhost',
+        port: 3003
+    },
+    watch: {
+      usePolling: true,
+    }
+  }  
 })
